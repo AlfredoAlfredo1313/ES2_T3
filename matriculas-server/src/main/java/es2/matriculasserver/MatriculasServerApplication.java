6a9e3c4a5301b1b3454abd1d7e670decfdb845ec
AlfredoAlfredo1313 <g.isdra@edu.pucrs.br>
2023-06-10 19:17:27 -0300

    Base

---
+++ matriculas-server/src/main/java/es2/matriculasserver/MatriculasServerApplication.java
@@ -0,0 +1,14 @@
+package es2.matriculasserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MatriculasServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculasServerApplication.class, args);
+	}
+
+}
